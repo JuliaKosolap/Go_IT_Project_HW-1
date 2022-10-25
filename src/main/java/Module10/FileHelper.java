9f638ad781744d4ca40e99d7fe8b518550e05a48
madwoman <jkosolap@gmail.com>
2022-10-25 14:59:25 +0300

    Module 10 Home Work added

---
+++ src/main/java/Module10/FileHelper.java
@@ -0,0 +1,142 @@
+package Module10;
+
+
+import java.io.*;
+import java.util.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class FileHelper {
+    public void printValidTelNumbersFromFile(File file) {
+        if (file.exists()) {
+            InputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scanner scanner = new Scanner(fis);
+            while (scanner.hasNext()) {
+                String telNumber = scanner.nextLine();
+                if (telNumber.matches("\\(?\\d+\\)?-?\\s?\\d+\\-\\d+")) {
+                    System.out.println(telNumber);
+                }
+            }
+        }
+    }
+
+    public void changeFileType(File file) {
+        if (file.exists()) {
+            InputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scanner scanner = new Scanner(fis);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            OutputStream fos = null;
+            try {
+                fos = new FileOutputStream("user.json");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            String pattern = "\\w+\\s\\d+";
+            while (scanner.hasNext()) {
+                String s = scanner.nextLine();
+                if (s.matches(pattern)) {
+                    String[] data = s.split(" ");
+                    String name = (data[0]);
+                    int age = Integer.parseInt(data[1]);
+                    Person person = new Person(name, age);
+                    String jsonString = gson.toJson(person);
+                    try {
+                        fos.write(jsonString.getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public void countNumbersOfWordsInFile(File file) {
+        if (file.exists()) {
+            InputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ArrayList<String> array = new ArrayList<>();
+            LinkedHashSet set = new LinkedHashSet();
+            Map<String, Integer> hashMap = new HashMap<>();
+            Scanner scanner = new Scanner(fis);
+            while (scanner.hasNext()) {
+                String line = scanner.next();
+                String[] words = line.split(" ");
+                for (String word :
+                        words) {
+                    array.add(word);
+                    set.add(word);
+                }
+            }
+            for (int i = 0; i < set.size(); i++) {
+                int count = 0;
+                String value = array.get(i);
+                for (int j = 0; j < array.size(); j++) {
+                    if (value.equals(array.get(j))) {
+                        count++;
+                    }
+                }
+                Word word = new Word(value, count);
+                hashMap.put(word.value, word.count);
+
+            }
+            Integer[] values = hashMap.values().toArray(new Integer[set.size()]);
+            Arrays.sort(values);
+            for (int j = values.length - 1; j >= 0; j--) {
+                for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                    if (entry.getValue().equals(values[j])) {
+                        System.out.println(entry.getKey() + " " + entry.getValue());
+                    }
+                }
+            }
+        }
+    }
+
+    private class Word {
+        private String value;
+        private int count;
+
+        Word(String word, int count) {
+            this.value = word;
+            this.count = count;
+        }
+    }
+
+    private class Person {
+        private String name;
+        private int age;
+
+
+        Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+
+    }
+}
+
+
--- src/main/java/Module10/FileReaderTest.java
+++ src/main/java/Module10/FileReaderTest.java
@@ -2,13 +2,17 @@ package Module10;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileReaderTest {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File file = new File("file.txt");
         System.out.println(file.exists());
         FileHelper helper = new FileHelper();
         helper.printValidTelNumbersFromFile(file);
-
+        File file2 = new File("file2.txt");
+        helper.changeFileType(file2);
+        File file3 = new File("words.txt");
+        helper.countNumbersOfWordsInFile(file3);
     }
 }
