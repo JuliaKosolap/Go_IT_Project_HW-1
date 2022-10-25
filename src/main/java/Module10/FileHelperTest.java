221271933d3e0c66c99bd18b1ca1fad858af36ce
madwoman <jkosolap@gmail.com>
2022-10-25 15:09:10 +0300

    Class renamed

--- src/main/java/Module10/FileHelperTest.java
+++ src/main/java/Module10/FileHelperTest.java
@@ -1,10 +1,9 @@
 package Module10;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class FileReaderTest {
+public class FileHelperTest {
     public static void main(String[] args) throws IOException {
         File file = new File("file.txt");
         System.out.println(file.exists());
